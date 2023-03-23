2dd10b88e951445bbf61b25b4c38e8999afbadaf
Oleksandr <kulykof@gmail.com>
2023-03-23 17:58:12 +0200

    add files

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-const date = new Date();
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-console.log(`Today is ${date.getFullYear()}`);
+program.parse(process.argv);
+
+const argv = program.opts();
+console.log(argv);
+
+const contacts = require("./contacts");
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      console.log(await contacts.listContacts());
+      break;
+
+    case "get":
+      console.log(await contacts.getContactById(id));
+      break;
+
+    case "add":
+      console.log(await contacts.addContact(name, email, phone));
+      break;
+
+    case "remove":
+      console.log(await contacts.removeContact(id));
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+invokeAction(argv);
